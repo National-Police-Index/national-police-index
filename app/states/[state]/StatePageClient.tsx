National-Police-Index/national-police-index

commit 4c84d494ec77560595b9b13acaa9e4a1d1381fdc

The NextJS Link component has been replaced with an HTML link element to facilitate navigation for back history purposes.

components/common/CursorPagination.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import styles from "./Pagination.module.scss";
 
@@ -120,7 +119,7 @@ export default function CursorPagination({
             }`}
           >
             {hasPreviousPage ? (
-              <Link
+              <a
                 href={!hasPreviousPage ? "#" : createPageUrl("prev")}
                 className={`block ${
                   !hasPreviousPage
@@ -130,7 +129,7 @@ export default function CursorPagination({
                 aria-disabled={!hasPreviousPage}
               >
                 <Prev disabled={false} />
-              </Link>
+              </a>
             ) : (
               <Prev disabled={true} />
             )}
@@ -148,7 +147,7 @@ export default function CursorPagination({
             }`}
           >
             {hasNextPage ? (
-              <Link
+              <a
                 href={!hasNextPage ? "#" : createPageUrl("next")}
                 className={`${
                   !hasNextPage
@@ -158,7 +157,7 @@ export default function CursorPagination({
                 aria-disabled={!hasNextPage}
               >
                 <Next disabled={false} />
-              </Link>
+              </a>
             ) : (
               <Next disabled={true} />
             )}

app/officers/[personNbr]/[full_name]/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import Link from "next/link";
 import { PoliceOfficer } from "@/types";
 import { useOfficerByPersonNbr } from "@/hooks/useOfficerByPersonNbr";
 import { useStaticText } from "@/hooks/useStaticText";
 import PageHeader from "@/components/PageHeader";
 import styles from "./page.module.scss";
 import { US_STATES } from "@/constants/states";
+
 
 type PoliceOfficerWithEventType = PoliceOfficer & {
   eventType: "Start" | "End" | "Discipline";
@@ -495,14 +495,14 @@ export default function OfficerProfilePage() {
                                 className={`flex-1 justify-start text-sm font-normal font-['Inter'] ${styles.timelineEvent}`}
                               >
                                 <b>
-                                  <Link
+                                  <a 
                                     href={`/agencies/${encodeURIComponent(
                                       event.agency_name
                                     )}`}
                                     className={styles.timelineAgency}
                                   >
                                     {event.agency_name}
-                                  </Link>
+                                  </a>
                                 </b>
                                 <small>
                                   {event.eventType === "Discipline" &&

components/common/Pagination.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
 import styles from './Pagination.module.scss';
@@ -133,7 +132,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
         {/* First page */}
         <li className={`${isFirstPage ? 'cursor-not-allowed' : ''} ${styles.arrow}`}>
           {isNotFirstPage ? 
-            (<Link
+            (<a
               href={isFirstPage ? '#' : createPageUrl(1)}
               className={`block ${isFirstPage
                 ? 'cursor-not-allowed opacity-50'
@@ -142,7 +141,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
               aria-disabled={isFirstPage}
             >
               <First disabled={false} />
-            </Link>) :
+            </a>) :
             (
               <First disabled={true} />
             )}
@@ -151,7 +150,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
         {/* Previous page */}
         <li className={`${isFirstPage ? 'cursor-not-allowed' : ''} ${styles.arrow}`}>
           {isNotFirstPage ? 
-            (<Link
+            (<a
               href={isFirstPage ? '#' : createPageUrl(prevPage)}
               className={`block ${isFirstPage
                 ? 'cursor-not-allowed opacity-50'
@@ -160,7 +159,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
               aria-disabled={isFirstPage}
             >
               <Prev disabled={false} />
-            </Link>) :
+            </a>) :
             (
               <Prev disabled={true} />
             )}
@@ -179,7 +178,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
           if (page !== currentPage) {
             return (
               <li key={page}>
-                <Link
+                <a
                   href={createPageUrl(page)}
                   className={`leading-tight ${currentPage === page
                     ? 'z-10 font-normal font-["Inter"] underline' // Should not happen with this logic, but kept for safety
@@ -187,7 +186,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
                     }`}
                 >
                   {page}
-                </Link>
+                </a>
               </li>
             )
           } else {
@@ -202,7 +201,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
         {/* Next page */}
         <li className={`${isLastPage ? 'cursor-not-allowed' : ''} ${styles.arrow}`}>
           {isNotLastPage ? 
-            (<Link
+            (<a
               href={isLastPage ? '#' : createPageUrl(nextPage)}
               className={`${isLastPage
                 ? 'cursor-not-allowed opacity-50'
@@ -211,7 +210,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
               aria-disabled={isLastPage}
             >
               <Next disabled={false} />
-            </Link>) :
+            </a>) :
             (
               <Next disabled={true} />
             )
@@ -221,7 +220,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
         {/* Last page */}
         <li className={`${isLastPage ? 'cursor-not-allowed' : ''} ${styles.arrow}`}>
           {isNotLastPage ?
-            (<Link
+            (<a
               href={isLastPage ? '#' : createPageUrl(totalPages)}
               className={`block ${isLastPage
                 ? 'cursor-not-allowed opacity-50'
@@ -230,7 +229,7 @@ export default function Pagination({ currentPage, totalPages, baseUrl }: Paginat
               aria-disabled={isLastPage}
             >
               <Last disabled={false} />
-            </Link>) :
+            </a>) :
             (
               <Last disabled={true} />
             )}

components/officers/OfficerCard.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useRef, useEffect, useState } from 'react';
-import Link from 'next/link';
 import { format } from 'date-fns';
 import { PoliceOfficer } from '@/types';
 import { useStaticText } from '@/hooks/useStaticText';
@@ -49,7 +48,7 @@ export default function OfficerCard({ officer }: OfficerCardProps) {
 
 
   return (
-    <Link
+    <a
       href={`/officers/${officer.document_id}/${officerSlug}`}
       className={`group flex w-full max-w-sm ${styles.officerCard} ${moreActive ? styles.moreActive : ''}`}
     >
@@ -84,6 +83,6 @@ export default function OfficerCard({ officer }: OfficerCardProps) {
           </button>
         )}
       </div>
-    </Link>
+    </a>
   );
 }

components/team/TeamCard.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import styles from './TeamCard.module.scss';
 
 interface TeamMember {
@@ -16,9 +15,9 @@ export default function TeamCard({ name, pronouns, description }: TeamMember) {
       if (linkMatch) {
         const [_, text, url] = linkMatch;
         return (
-          <Link key={index} href={url} className={`text-[#122823] text-sm font-normal font-['Inter'] underline leading-tight hover:text-[#4F8C7E] ${_}`}>
+          <a key={index} href={url} className={`text-[#122823] text-sm font-normal font-['Inter'] underline leading-tight hover:text-[#4F8C7E] ${_}`}>
             {text}
-          </Link>
+          </a>
         );
       }
       return (

commit f260a9a5ffd7db7a5fac6baf50d562dd9e2e00e8

Cursor pagination: bug introduced in https://github.com/National-Police-Index/national-police-index/commit/4c84d494ec77560595b9b13acaa9e4a1d1381fdc#diff-e3667aed61586b22b8a8347d00473465671b85767a935a1f908ce585868bc4d9

app/states/[state]/StatePageClient.tsx
@@ -225,6 +225,8 @@ export default function StatePageClient() {
                         onPageSizeChange={(newSize) => {
                           const params = new URLSearchParams(searchParams.toString());
                           params.set('pageSize', newSize.toString());
+                          params.set('page', "1");
+                          params.set('direction', "");
                           
                           // Track page size change
                           trackPagination(apiCurrentPage, stateData.name, totalGroups);

components/common/CursorPagination.tsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import styles from "./Pagination.module.scss";
+import Link from "next/link";
 
 interface CursorPaginationProps {
   currentPage: number;
@@ -147,7 +148,7 @@ export default function CursorPagination({
             }`}
           >
             {hasNextPage ? (
-              <a
+              <Link
                 href={!hasNextPage ? "#" : createPageUrl("next")}
                 className={`${
                   !hasNextPage
@@ -157,7 +158,7 @@ export default function CursorPagination({
                 aria-disabled={!hasNextPage}
               >
                 <Next disabled={false} />
-              </a>
+              </Link>
             ) : (
               <Next disabled={true} />
             )}